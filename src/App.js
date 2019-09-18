f7445a6544d16c22702396d889dc0ca0eed7b9de
kct285 <kimberlytanchay@Kimberlys-MacBook-Pro.local>
2019-09-18 10:49:28 +0800

    personalized html

--- src/App.js
+++ src/App.js
@@ -4,22 +4,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+   <div>
+      < img id="pic1" 
+      alt="bear standing in river"
+      src="https://upload.wikimedia.org/wikipedia/commons/5/5d/Kamchatka_Brown_Bear_near_Dvuhyurtochnoe_on_2015-07-23.jpg"/>
+      <h1>Hi! I'm Barry, the Brown Bear</h1>
+
+      <img id="bee" src="https://i.pinimg.com/originals/02/ce/79/02ce7945c01a4d62f78e480ca9c51f00.gif" width="100px" />
+
+      <div id="profile">
+        <h2>PROFILE</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A friendly neighbourhood bear looking for new friends :) 
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div id="hobbies">
+      <h2> HOBBIES</h2>
+      <ul>
+        <li>Fishing for salmon</li>
+        <li>Hunting for berries and honey </li>
+        <li>Hibernating for most of my life </li>
+      </ul>
+      </div> 
     </div>
+
   );
 }
 
